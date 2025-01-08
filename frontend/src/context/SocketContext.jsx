5a6f04c143db1d8749d538f53f1e16ce97289f60
Hemant Kumar <hemant.k2807@gmail.com>
2025-01-09 02:32:00 +0530

    Guftagu URL Updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://guftagu.onrender.com/", {
+      const socket = io("https://guftagu-rjks.onrender.com", {
         query: {
           userId: authUser._id,
         },
