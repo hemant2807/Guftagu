84899d6ee0f50f7c3c870b386a40fdcee46e347e
Hemant Kumar <hemant.k2807@gmail.com>
2025-01-09 02:27:39 +0530

    SignUp page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-// import SignUp from "./pages/signup/SignUp.jsx";
-import SignUp from "./pages/signup/signup.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext.jsx";
 
